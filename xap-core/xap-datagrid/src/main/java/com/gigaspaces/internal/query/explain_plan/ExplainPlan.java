6f27727d3aad7ec8a77e13d3ac813eb76e1efa60
yaelna <yaeln@gigaspaces.com>
2016-08-21 12:17:08 +0300

    XAP-12854 rebased from master

--- xap-core/xap-datagrid/src/main/java/com/gigaspaces/internal/query/explain_plan/ExplainPlan.java
+++ xap-core/xap-datagrid/src/main/java/com/gigaspaces/internal/query/explain_plan/ExplainPlan.java
@@ -1,6 +1,8 @@
 package com.gigaspaces.internal.query.explain_plan;
 
+import com.gigaspaces.internal.query.ICustomQuery;
 import com.gigaspaces.internal.server.storage.IEntryData;
+import com.gigaspaces.internal.server.storage.TemplateEntryData;
 
 /**
  * @author yael nahon
@@ -9,8 +11,19 @@ import com.gigaspaces.internal.server.storage.IEntryData;
 public class ExplainPlan {
 
     private QueryOperationNode root;
+    private TemplateEntryData entryData;
 
     public ExplainPlan(IEntryData template) {
+        this.entryData = (TemplateEntryData) template;
+        buildQueryTree();
+    }
 
+    private void buildQueryTree() {
+        QueryOperationNode currentNode = getNode(entryData.getCustomQuery());
+//        if(entryData.getCustomQuery())
+    }
+
+    private QueryOperationNode getNode(ICustomQuery customQuery) {
+        return null;
     }
 }

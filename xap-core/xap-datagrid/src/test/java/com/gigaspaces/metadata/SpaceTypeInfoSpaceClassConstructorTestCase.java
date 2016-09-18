b6361dfc4e88fbd889a929e4f6e1c24b680eb502
Barak Bar Orion <barak.bar@gmail.com>
2016-09-18 16:53:33 +0300

    Add license header.

--- xap-core/xap-datagrid/src/test/java/com/gigaspaces/metadata/SpaceTypeInfoSpaceClassConstructorTestCase.java
+++ xap-core/xap-datagrid/src/test/java/com/gigaspaces/metadata/SpaceTypeInfoSpaceClassConstructorTestCase.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2008-2016, GigaSpaces Technologies, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.gigaspaces.metadata;
 
 import com.gigaspaces.internal.metadata.SpacePropertyInfo;
@@ -11,8 +27,8 @@ import junit.framework.Assert;
 import junit.framework.TestCase;
 
 /**
- * @author: yaeln
- * @since: 12.0.0
+ * Created by Yaeln
+ * @since 12.0
  */
 
 public class SpaceTypeInfoSpaceClassConstructorTestCase extends TestCase {

6ed240dd6cbe1a4572ac17c26f6fd69c165839a1
nivin <niv@gigaspaces.com>
2017-03-07 17:40:13 +0200

    XAP-13021 - zookeeper ports configurable

--- xap-core/xap-datagrid/src/main/java/com/j_spaces/kernel/SystemProperties.java
+++ xap-core/xap-datagrid/src/main/java/com/j_spaces/kernel/SystemProperties.java
@@ -749,6 +749,8 @@ public class SystemProperties extends CommonSystemProperties {
     public static final String MANAGER_REST_SSL_KEYSTORE_PASSWORD = "com.gs.manager.rest.ssl.keystore-password";
     public static final String MANAGER_REST_REQUESTS_MAX_RUNNING = "com.gs.manager.rest.max-running-requests";
     public static final String MANAGER_REST_REQUESTS_MAX_COMPLETED = "com.gs.manager.rest.max-completed-requests";
+    public static final String MANAGER_ZOOKEEPER_DISCOVERY_PORT = "com.gs.manager.zookeeper.discovery.port";
+    public static final String MANAGER_ZOOKEEPER_LEADER_ELECTION_PORT = "com.gs.manager.zookeeper.leader-election.port";
 
     private static final Map<String, String> defaultValues = initDefaultValues();
 

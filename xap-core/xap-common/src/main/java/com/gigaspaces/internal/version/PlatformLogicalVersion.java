abed59c5ee9ba87f41bd37dc34cc9ae304415cff
nivin <niv@gigaspaces.com>
2017-03-29 14:15:55 +0300

    Final version for 12.1 GA

--- xap-core/xap-common/src/main/java/com/gigaspaces/internal/version/PlatformLogicalVersion.java
+++ xap-core/xap-common/src/main/java/com/gigaspaces/internal/version/PlatformLogicalVersion.java
@@ -233,7 +233,7 @@ public class PlatformLogicalVersion implements Externalizable {
     public static final PlatformLogicalVersion v11_0_1 = new PlatformLogicalVersion(11, 0, 1, 14890, 0);
     public static final PlatformLogicalVersion v12_0_0 = new PlatformLogicalVersion(12, 0, 0, 15790, 0);
     public static final PlatformLogicalVersion v12_0_1 = new PlatformLogicalVersion(12, 0, 1, 16600, 0);
-    public static final PlatformLogicalVersion v12_1_0 = new PlatformLogicalVersion(12, 1, 0, 16701, 0);
+    public static final PlatformLogicalVersion v12_1_0 = new PlatformLogicalVersion(12, 1, 0, 17000, 0);
 
     //DOCUMENT BACKWARD BREAKING CHANGES, EACH CHANGE IN A LINE
     //GS-XXXX: Short backward breaking description and classes

6679b9a9be136fa62383ce4b42b107370bc4c716
ypgit <yechielf@gigaspaces.com>
2017-08-29 13:15:04 +0300

    XAP-13294 cater for request for non-optimized entry when the entry in OffHeapRefEntryCacheInfo is optimized- need to reread it

--- xap-core/xap-datagrid/src/main/java/com/j_spaces/core/cache/offHeap/OffHeapRefEntryCacheInfo.java
+++ xap-core/xap-datagrid/src/main/java/com/j_spaces/core/cache/offHeap/OffHeapRefEntryCacheInfo.java
@@ -381,11 +381,11 @@ public class OffHeapRefEntryCacheInfo
                 if (res.getBulkInfo() != null && res.getBulkInfo().isActive() && res.getBulkInfo().getOwnerThread() != Thread.currentThread()) {
                     throw BusyInBulkIndicator;
                 }
-                //entry can be pinned only after bulk-flush when its not unpinned yet from after-bult op
-                //in BlobStoreBulkInfo- but it can be dirty
-                if (attach && !isPinned())
-                    throw new RuntimeException("entry attach but not pinned " + _m_Uid);
-                if (attach && isDirty())
+                //entry can be pinned only after bulk-flush when its not unpinned yet from after-bulk op
+                //in BlobStoreBulkInfo- but it can not be dirty
+                if (!isPinned())
+                    throw new RuntimeException("entry attach and entry in RefEntryCacheInfo but not pinned " + _m_Uid);
+                if (isDirty())
                     throw new RuntimeException("entry attach but dirty" + _m_Uid);
                 if (!onlyIndexesPart && res.isOptimizedEntry())
                 {//entry pinned after bulk flush i will unload it +  unpin and it will be pinned again after reading
